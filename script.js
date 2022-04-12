fd6557be6fb6c2466e9236bb7b08ec5bfcf7da04
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-12 16:41:43 +0600

    js object

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+// let person = "John Doe";
+// document.getElementById("demo").innerHTML = person;
 
-console.log("Hello World")
+// let person = {
+//     firstName : "John",
+//     lastName  : "Doe",
+//     age     : 50,
+//     eyeColor  : "blue"
+//   };
+  
+//   document.getElementById("demo").innerHTML = person.firstName + " " + person.lastName;
+
+// JavaScript Object Using an Object Literal(For readability, simplicity and execution speed, use the object literal method.)
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+  
+//   document.getElementById("demo").innerHTML =
+//   person.firstName + " is " + person.age + " years old.";
+
+//Using the JavaScript Keyword new
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue"; 
+
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
